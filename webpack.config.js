2cfcc4f74cde4dc665388011d179ab2a512960c5
Lucian Fialho <lucianfialhobp@hotmail.com>
2025-02-24 13:53:52 -0300

    -- mudando arquivo de saida

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/main.js', // ponto de entrada da sua lib
   output: {
-    filename: 'http-error-tracker.umd.js', // nome do bundle gerado
+    filename: 'err-alytics.umd.js', // nome do bundle gerado
     path: path.resolve(__dirname, 'dist'),
     library: 'HttpErrorTracker', // nome da variável global quando usado via CDN
     libraryTarget: 'umd', // formato UMD para compatibilidade universal
